B10 program, calculation of erros plus other small tweaks on A ( fixed a4)

src/a.py
@@ -5,8 +5,9 @@ from scipy.signal import find_peaks as fp
 class A:
     '''
     Class to read data from EV_2022.A10 file
+    Format: t;a1;a2;a3;a4
     '''
-    def __init__(self, n, style = "" ):
+    def __init__(self, n):
         '''
         n is the number of the acceleration to study
         eg: a3, n = 3
@@ -15,7 +16,6 @@ class A:
         self.t = self.collect_data(0) # collect time
         self.n = n
         self.a = self.collect_data(n) # collect acceleration values
-        self.style = style            # style for the plots if none given reverts to classic
         self.magn = 0
         self.feq = 0
         self.peaksf = np.array([])
@@ -32,7 +32,7 @@ class A:
             for i in f:
                 i = i.split(';')             # split the values by ';'
                 if '\n' in i[n]:
-                    i = i[n].split('\n')[0]
+                    i[n] = i[n].split('\n')[0]
                 try:
                     i = float(i[n])
                     a.append(i)
@@ -45,13 +45,12 @@ class A:
         Plot the graph for a given acceleration
         '''
         plt.figure("a"+str(self.n))
-        plt.plot(self.t, self.a, self.style)
+        plt.plot(self.t, self.a)
         plt.title("Variação temporal da aceleração " + str(self.n))
         plt.xlabel('t [s]') 
         plt.ylabel('a'+ str(self.n) +' [m/s^2]') 
         plt.autoscale() 
         plt.savefig("../results/A10/a"+str(self.n)+".png")
-        #plt.show()
 
     def single_sided_magnitude_spectrum(self):
         '''
@@ -60,7 +59,7 @@ class A:
         plt.figure("Espetro a"+str(self.n))
         out = plt.magnitude_spectrum(self.a)
         self.magn, self.freq = out[0], out[1]
-        peaks, _ = fp(self.magn,height = 0.5)
+        peaks, _ = fp(self.magn,height = 0.25)
         max_index = np.where(self.magn == np.amax(self.magn))
         if max_index not in peaks:
             peaks = np.append(max_index, peaks)
@@ -73,7 +72,6 @@ class A:
         plt.ylabel('|a'+ str(self.n) +'(t)|') 
         plt.autoscale() 
         plt.savefig("../results/A10/espetro_a"+str(self.n)+".png")
-        #plt.show()
 
     def get_results_peaks(self):
         return self.peaksf, self.peaksm