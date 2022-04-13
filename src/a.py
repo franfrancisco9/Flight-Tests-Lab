8c7bf365635f44eec2a925bca604905cc78b262d
Francisco <francisco.parreira.fonseca@gmail.com>
2022-04-13 22:01:50 +0100

    finalize a, latex tabels of peaks and setup for B values

--- src/a.py
+++ src/a.py
@@ -40,19 +40,10 @@ class A:
                     continue
         return a
     
-    def graph(self, t = []):
+    def graph(self):
         '''
         Plot the graph for a given acceleration
         '''
-        # if t != []:
-        #     a = []
-        #     for i in range(len(t)):
-        #         for j in range(len(self.t)):
-        #             if t[i] == self.t[j]:
-        #                 a.append(self.a[j])
-        #     self.t = t
-        #     self.a = a
-
         plt.figure("a"+str(self.n))
         plt.plot(self.t, self.a, self.style)
         plt.title("Variação temporal da aceleração " + str(self.n))
@@ -60,7 +51,7 @@ class A:
         plt.ylabel('a'+ str(self.n) +' [m/s^2]') 
         plt.autoscale() 
         plt.savefig("../results/A10/a"+str(self.n)+".png")
-        plt.show()
+        #plt.show()
 
     def single_sided_magnitude_spectrum(self):
         '''
@@ -68,25 +59,21 @@ class A:
         '''
         plt.figure("Espetro a"+str(self.n))
         out = plt.magnitude_spectrum(self.a)
-        self.magn = out[0]
-        self.freq = out[1]
+        self.magn, self.freq = out[0], out[1]
         peaks, _ = fp(self.magn,height = 0.5)
         max_index = np.where(self.magn == np.amax(self.magn))
         if max_index not in peaks:
             peaks = np.append(max_index, peaks)
+        self.peaksf, self.peaksm = self.freq[peaks], self.magn[peaks]
 
-        self.peaksf = self.freq[peaks]
-        self.peaksm = self.magn[peaks]
         plt.plot(self.freq, self.magn)
         plt.plot( self.peaksf, self.peaksm, "x")
-
         plt.title("Espetro unilateral de amplitude da aceleração " + str(self.n))
         plt.xlabel('Frequency [Hz]') 
         plt.ylabel('|a'+ str(self.n) +'(t)|') 
         plt.autoscale() 
         plt.savefig("../results/A10/espetro_a"+str(self.n)+".png")
-        plt.show()
-        
-if __name__ == '__main__':  
-    a1 = A(1)
-    a1.single_sided_magnitude_spectrum()
+        #plt.show()
+
+    def get_results_peaks(self):
+        return self.peaksf, self.peaksm
--- src/main.py
+++ src/main.py
@@ -1,25 +1,59 @@
 import numpy as np
 from a import A
+from tabulate import *
 
+def exercise_a():
+    '''
+    function that generates all the necessary results to answer exercise A
+    '''
+    accelerations = {'a1':A(1), 'a2':A(2), 'a3':A(3), 'a4':A(4)} 
+
+    # acceleration 1 results:
+    accelerations['a1'].graph()
+    accelerations['a1'].single_sided_magnitude_spectrum()
+    a1peaksf, a1peaksm = accelerations['a1'].get_results_peaks()
+
+    # acceleration 2 results:
+    accelerations['a2'].graph()
+    accelerations['a2'].single_sided_magnitude_spectrum()
+    a2peaksf, a2peaksm = accelerations['a2'].get_results_peaks()
+
+    # acceleration 3 results:
+    accelerations['a3'].graph()
+    accelerations['a3'].single_sided_magnitude_spectrum()
+    a3peaksf, a3peaksm = accelerations['a3'].get_results_peaks()
+
+    # acceleration 4 results:
+    accelerations['a4'].graph()
+    accelerations['a4'].single_sided_magnitude_spectrum()
+    a4peaksf, a4peaksm = accelerations['a4'].get_results_peaks()
+
+    for i in range(4):
+        with open("../results/A10/peaks_results_a"+str(i)+".tex", 'w') as f:
+            rows = []
+            row_header = ["Picos de a"+str(i+1)] 
+            row_f = ["Frequência [Hz]"]
+            row_m = ["Magnitude"]
+            for j in range(len(accelerations['a'+str(i+1)].get_results_peaks()[0])):
+                row_header.append("Pico " + str(j))
+                row_f.append(accelerations['a'+str(i+1)].get_results_peaks()[0][j])
+                row_m.append(accelerations['a'+str(i+1)].get_results_peaks()[1][j])
+                
+            rows = [row_header, row_f, row_m]
+            table = tabulate(rows, headers='firstrow', tablefmt='latex')
+            f.write(table)
+
+def exercise_b():
+    '''
+    function that generates all the necessary results to answer exercise b
+    '''           
+    return; 
 
 def main():
-    '''main function'''
-    a1 = A(1)
-    a2 = A(2)
-    a3 = A(3)
-    a4 = A(4)
-
-    a1.graph()
-    a1.single_sided_magnitude_spectrum()
-    a2.graph()
-    a2.single_sided_magnitude_spectrum()
-    a3.graph()
-    a3.single_sided_magnitude_spectrum()
-    a4.graph()
-    a4.single_sided_magnitude_spectrum()
-    print(a4.peaksf)
-    print(a4.peaksm)
-
+    '''
+    main function
+    '''
+    exercise_a()
 
 
 if __name__ == '__main__':
